0f77bfb43d2b9e6877fd6d99ff3cc7807a42f9a9
Dblmov <74150483+Dblmov@users.noreply.github.com>
2020-12-29 13:11:41 +0300

    1.5

---
+++ 1.3/JS/script.js
@@ -0,0 +1,29 @@
+if (window.innerWidth <= 320) {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 'auto',
+        // slidesPerColumn: 1,
+        // slidesPerColumnFill:"row",
+        spaceBetween: 16,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true, 
+        },
+    });
+};
+
+
+document.querySelector('.brand__show--all').addEventListener('click', function () {
+    var wrapper = document.querySelector('.swiper-wrapper');
+    var btn = document.querySelector('.brand__show--all');
+
+    if (wrapper.classList.contains('show')) {
+        btn.textContent = 'Показать всё';
+        btn.classList.remove('show');
+        wrapper.classList.remove('show');
+    } else {
+        btn.textContent = 'Скрыть';
+        btn.classList.add('show');
+        wrapper.classList.add('show');
+    };
+})
